3b976c3fd98db45e15f565d21273401765a45a77
Alex Dreyer <alex.dreyer@cmgdigital.com>
2012-05-16 14:56:15 -0400

    add the demo app to installed apps and fix TextField

--- parker_demo/demo/models.py
+++ parker_demo/demo/models.py
@@ -3,6 +3,6 @@ from django.db import models
 class ParkerDemo(models.Model):
     title = models.CharField(max_length=140)
     summary = models.TextField(max_length=1000)
-    link = models.textField(max_length=1000)
+    link = models.CharField(max_length=1000)
 
 
