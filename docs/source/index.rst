5dbb6c43d8cd29abac89b1acd6677d4567bf10ab
Alex Dreyer <alex.dreyer@cmgdigital.com>
2012-06-20 14:31:39 -0400

    some basic documentation for the templatetag

--- docs/source/index.rst
+++ docs/source/index.rst
@@ -6,6 +6,14 @@
 Welcome to Parker's documentation!
 ==================================
 
+Parker is designed to make realtime publishing from django easy. It puts marimo_ widgets in pages and communicates with them via browsermq_ 
+
+
+
+
+.. _marimo: http://github.com/coxmediagroup/marimo
+.. _browsermq: http://github.com/coxmediagroup/parker
+
 Contents:
 
 .. toctree::
@@ -13,6 +21,7 @@ Contents:
 
    carriers
    listeners
+   templatetag
 
 
 Indices and tables
--- parker/templatetags/parker_tags.py
+++ parker/templatetags/parker_tags.py
@@ -1,3 +1,17 @@
+"""parker.templatetags.parker_tags.
+The parker template tag exists to get a widget for a carrier and put it in the page
+
+.. autofunction:: parker.templatetags.parker_tags.parker_tag
+
+
+Example
+_________
+
+.. literalinclude:: ../../parker_demo/templates/page1.html
+"""
+
+
+
 import django
 if django.VERSION < (1, 4, 0):
     from simple_tag.base_template import Library
@@ -11,4 +25,15 @@ register = Library()
 
 @register.simple_tag(name='parker')
 def parker_tag(carrier, widget_id, prototype=None, template=None, queues=None, initialize=False):
+    """ The parker tag looks in the carrier library and gets widget code from it.
+
+    :param carrier: The name of the carrier this wisget should use.
+    :param widget_id: The id this widget should have in marimo.
+    :keyword prototype: The widget prototype to use.
+    :keyword template: The mustache template to pass to the widget.
+    :keyword queues: The queues this widget should subscribe to.
+    :keyword initialize: Should this widget initialize itself.
+
+    All keywords default to the values of the carrier.
+    """
     return parker_lib[carrier].get_widget(widget_id, prototype, template, queues, initialize)
